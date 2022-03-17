8f366a743448a4ff624fe163af39ebfda74fee32
Pedro Lucas <92189406+BitGigbits@users.noreply.github.com>
2022-03-17 16:18:48 -0300

    Update Tesouro.c

--- Tesouro.c
+++ Tesouro.c
@@ -2,6 +2,10 @@
 #include <string.h>
 #define MAX 250
 
+//Projeto -> Existem tesouros submersos em que são definidos pelos pares (Profundidade, Quantidade de Ouro). 
+//Coletar esses tesouros levam 9 * Profundidade de tempo em segundos. Existe um cilindro de ar que permite ficar exatamente t segundos submerso.
+//Determinar a quantidade total de tesouro possível a ser coletado dado um conjunto de tesouros e uma entrada t de tempo.
+
 int ouro[] = {25, 45, 75};
 int prof[] = {4, 6, 8};
 int t_sub = 108;
